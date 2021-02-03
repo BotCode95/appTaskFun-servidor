7c2afc380a37154bee91b482a97a659f5950d7a5
BotCode95 <patribottino95@gmail.com>
2021-02-02 21:09:00 -0300

    listen ok

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-// const PORT = localhost:4000;
+//habilitar express.json
+app.use(express.json({ extended : true}));
 
-app.listen(PORT, (req,res) => {
-    console.log(`Esta escuchando en el port ${PORT}`);
+const PORT = process.env.PORT || 4000;
+
+//init app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 })
